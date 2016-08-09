9b8b5ac929875f0570a26a6a94b77a708b0a63b7
FacundoTaramasco <f.taramasco@hotmail.com.ar>
2016-08-08 21:10:08 -0300

    comenzando logica servidor (2)

--- src/java/com/trucoargento/core/Servidor.java
+++ src/java/com/trucoargento/core/Servidor.java
@@ -8,6 +8,12 @@ package com.trucoargento.core;
 import com.trucoargento.modelo.Jugador;
 import com.trucoargento.modelo.Truco;
 import java.io.IOException;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.spi.JsonProvider;
 import javax.websocket.CloseReason;
 import javax.websocket.Endpoint;
 import javax.websocket.OnClose;
@@ -37,8 +43,14 @@ public class Servidor {
         if (t.getJugadorUno().getSesion() == null) {
             t.getJugadorUno().setSesion(s);
             System.out.println("Entro jugador uno");
-            try {
-                s.getBasicRemote().sendText("Esperando al jugador dos...");
+            try {                
+                JsonProvider provider = JsonProvider.provider();
+                JsonObject mensajeJson = provider.createObjectBuilder()
+                    .add("accion", "espera")
+                    .add("mensaje", "esperando jugador Dos")
+                    .build();
+                s.getBasicRemote().sendText(mensajeJson.toString());
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -47,8 +59,15 @@ public class Servidor {
             t.getJugadorDos().setSesion(s);
             System.out.println("Entro jugador dos");
             try {
-                t.getJugadorUno().getSesion().getBasicRemote().sendText("El juego comenzará");
-                t.getJugadorDos().getSesion().getBasicRemote().sendText("El juego comenzará");
+                JsonProvider provider = JsonProvider.provider();
+                JsonObject mensajeJson = provider.createObjectBuilder()
+                    .add("accion", "a_jugar")
+                    .add("href", "pantallaJuego.html")
+                    //.add("mensaje", "el juego comenzara!")
+                    .build();
+
+                t.getJugadorUno().getSesion().getBasicRemote().sendText(mensajeJson.toString());
+                t.getJugadorDos().getSesion().getBasicRemote().sendText(mensajeJson.toString());
             } catch(IOException e) {
                 e.printStackTrace();
             }
@@ -62,13 +81,30 @@ public class Servidor {
     public String onMessage(String mensaje, Session s) {
         
         // mensaje de jugador uno
-        if (t.getJugadorUno().getSesion() != null && t.getJugadorUno().getSesion() == s) {
+        if (t.getJugadorUno().getSesion() == s) {
             
+            JsonReader reader = Json.createReader(new StringReader(mensaje));
+            JsonObject mensajeJson = reader.readObject();
+            
+            if (mensajeJson.getString("accion").equals("entraJuego")) {
+                // seteando nombre del jugador
+                t.getJugadorUno().setNombre( mensajeJson.getString("nombre") );
+                System.out.println("jugador uno se llama : " +  mensajeJson.getString("nombre"));
+            }
+
         }
         
         // mensaje de jugador dos
-        if (t.getJugadorDos().getSesion() != null && t.getJugadorDos().getSesion() == s) {
+        if (t.getJugadorDos().getSesion() == s) {
             
+            JsonReader reader = Json.createReader(new StringReader(mensaje));
+            JsonObject mensajeJson = reader.readObject();
+            
+            if (mensajeJson.getString("accion").equals("entraJuego")) {
+                // seteando nombre del jugador
+                t.getJugadorDos().setNombre( mensajeJson.getString("nombre") );
+                System.out.println("jugador dos se llama : " +  mensajeJson.getString("nombre"));
+            }
         }
  
         return "";
